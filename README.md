2c487783ecd6f2fc5bdddca54a7c35fb01643f98
Frederic PARREL <fredericparrel@gmail.com>
2019-01-31 07:42:38 +0100

    Commit of last night changes: allow to output results in geojson + osm relation handling added + remove roads from natural aeras

--- README.md
+++ README.md
@@ -1,33 +1,78 @@
 # osmaeras
-Compute aeras and enghts from OpenStreetMap data
 
-# Step-by-step
+Compute aeras and lenghts from OpenStreetMap data
 
-## Get osm data
+## Step-by-step
+
+### Get osm data
 `wget http://download.geofabrik.de/europe/france/provence-alpes-cote-d-azur-latest.osm.pbf`
 
-## Install Rust + cargo
+### Install Rust + cargo
 ```
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 ```
 
-## Install osmosis
-TODO
+### Install osmosis
+```
+wget https://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
+mkdir osmosis
+mv osmosis-latest.tgz osmosis
+cd osmosis
+tar xvfz osmosis-latest.tgz
+rm osmosis-latest.tgz
+chmod a+x bin/osmosis
+```
 
-## Build way/aera extractor
+### Install python dependencies
+`sudo pip install shapely pyproj geopandas`
+
+### Build way/aera extractor
 ```
 cd osmextract
 cargo build --release
 cd ..
 ```
 
-## Get border of the zone you want
+### Get border of the zone you want
 `./osmextract/target/release/osmextractfromname provence-alpes-cote-d-azur-latest.osm.pbf "Technopole de Sophia-Antipolis" > sophia.poly`
 
-## Get a subset within borders
+### Get a subset within borders
 `osmosis/bin/osmosis --read-pbf provence-alpes-cote-d-azur-latest.osm.pbf --bounding-polygon file="sophia.poly" completeWays=yes --write-xml file="sophia.osm"`
 
-## Compute aeras and length
+### Compute aeras and length
 `python comp.py`
+
+## Last results
+
+Correction des erreurs (a ignorer si moins d'un kilometre)
+  Chemins hors frontieres = 159 m
+  Routes hors urbanisation= -80 m
+
+Lineaire:
+
+  chemins et pistes = 249.3 km
+  rivieres et canaux = 41.4 km
+  routes = 212.3 km
+
+Superficies:
+
+  Urbanisation: 596 ha
+    dont batiments = 106 ha
+    dont parkings = 36 ha
+    dont routes = 103 ha
+
+  Espaces verts artificiels: 105 ha
+    dont golfs = 84.3 ha
+    dont agricole = 5.6 ha
+    dont espaces verts = 2.3 ha
+    dont reservoirs et piscines = 0.7 ha
+    dont terrains de sport (hors golf) = 12.5 ha
+
+  Espaces naturels = 1499 ha
+    dont foret, garrigue, prairie = 1533 ha
+    dont eau (lacs, etangs), hors rivieres etroites = 2 ha
+
+superficie totale = 2400 ha
+superficie totale identifiee = 2189 ha
 
--- comp.py
+++ comp.py
@@ -2,16 +2,14 @@ import xml.etree.ElementTree as ET
 from math import sin, cos, atan2, sqrt, pi, radians
 from shapely.geometry import Polygon, LineString
 from shapely.ops import cascaded_union
-
-data = ET.parse('sophia.osm')
-
-nodes = {}
-for e in data.getroot():
-  if e.tag=='node':
-    nodes[e.attrib['id']] = float(e.attrib['lat']),float(e.attrib['lon'])
+import shapely.ops
+import pyproj
+from functools import partial
+import geopandas
+import json
 
 def GeodeticDistGreatCircle(lat1,lon1,lat2,lon2):
-  "Compute distance between two points of the earth geoid (approximated to a sphere)"
+  """Compute distance between two points of the earth geoid (approximated to a sphere)"""
   # convert inputs in degrees to radians
   lat1 = lat1 * 0.0174532925199433
   lon1 = lon1 * 0.0174532925199433
@@ -29,29 +27,21 @@ def lenOfPath(path):
     l += GeodeticDistGreatCircle(path[i-1][0],path[i-1][1],path[i][0],path[i][1])
   return l
 
-def lenOfPathWithin(path,bounds):
+#def lenOfPathWithin(path,bounds):
+#  l = LineString(reproject(path))
+#  bounds = Polygon(reproject(bounds))
+#  l.intersection(bounds)
+#  return l.length
+
+def lenOfPathWithinPolygon(path,boundsshapely):
   l = LineString(reproject(path))
-  bounds = Polygon(reproject(bounds))
-  l.intersection(bounds)
+  l.intersection(boundsshapely)
   return l.length
-
-def lenOfPathWithinPolygon(path,bounds):
-  l = LineString(reproject(path))
-  l.intersection(bounds)
-  return l.length
-
-
-#  l = 0.0
-#  for i in range(1,len(path)):
-#    Line(path[i-1][0],path[i-1][1],path[i][0],path[i][1])
 
 def reproject(p):
   """Returns the x & y coordinates in meters using a sinusoidal projection"""
-  earth_radius = 6371009 # in meters
-  lat_dist = pi * earth_radius / 180.0
-  y = [pt[0] * lat_dist for pt in p]
-  x = [pt[1] * lat_dist * cos(radians(pt[0])) for pt in p]
-  return zip(x, y)
+  out = [pyproj.transform(proj_latlon,proj4area,pt[1],pt[0]) for pt in p]
+  return out
 
 def areaOfPolygon(p1):
   """Calculates the area of an arbitrary polygon given its verticies"""
@@ -63,15 +53,62 @@ def areaOfPolygon(p1):
     area += p[i][0] * (p[i+1][1] - p[i-1][1])
   return abs(area) / 2.0
 
-paths_len = 0.0
-roads_aera = 0.0
-roads_len = 0.0
+def boundingBox(bounds):
+  return min(lat for lat,lon in bounds),max(lat for lat,lon in bounds),min(lon for lat,lon in bounds),max(lon for lat,lon in bounds)
+
+def deprojGeom(geom):
+  deproj = partial(pyproj.transform, proj4area, proj_latlon)
+  return shapely.ops.transform(deproj,geom)
+
+def togeojson():
+  urb = geopandas.GeoSeries(map(deprojGeom,urbanisation)).__geo_interface__['features']
+  for feature in urb:
+    feature['properties'] = {'color':'grey'}
+    del feature['id']
+  art = geopandas.GeoSeries(map(deprojGeom,artificial_green)).__geo_interface__['features']
+  for feature in art:
+    feature['properties'] = {'color':'yellow'}
+    del feature['id']
+  nat = geopandas.GeoSeries(map(deprojGeom,natural)).__geo_interface__['features']
+  for feature in nat:
+    feature['properties'] = {'color':'green'}
+    del feature['id']
+  bbox = boundingBox(bounds)
+  bds = {'type': 'Feature','properties': {'color':'red','fill':'transparent'},'geometry':{'type':'Polygon','coordinates':[map(lambda xy:(xy[1],xy[0]),bounds+[bounds[0]])]},'bbox':[bbox[2],bbox[0],bbox[3],bbox[1]]}
+  doc = {'type': 'FeatureCollection', 'features':urb+art+nat+[bds]}
+  f = open('zonessophia.geojson','w')
+  json.dump(doc,f)
+  f.close()
+
+proj_latlon = pyproj.Proj(proj='latlong',datum='WGS84')
+proj4area = pyproj.Proj("+proj=sinu +R=6371009")
+
+print 'Lecture du fichier et mise en cache...'
+data = ET.parse('sophia.osm')
+
+nodes = {}
+for e in data.getroot():
+  if e.tag=='node':
+    nodes[e.attrib['id']] = float(e.attrib['lat']),float(e.attrib['lon'])
+
+ways = {}
+for e in data.getroot():
+  if e.tag=='way':
+    ways[e.attrib['id']] = map(lambda sube:nodes[sube.get('ref')], filter(lambda sube:sube.tag=='nd',e))
+
+print 'Analyse...'
+
+#paths_len = 0.0
+#roads_aera = 0.0
+#roads_len = 0.0
 paths = []
 roads = []
+roadssh = []
 buildings = []
 built_zones = []
 forests = []
 golfs = []
+stades = []
 lakes = []
 reservoirs = []
 farming = []
@@ -94,9 +131,10 @@ for e in data.getroot():
     if tags.has_key('highway'):
       if tags['highway'] in ('path','track','steps','bridleway','footway','cycleway'):
         paths.append(path)
-        paths_len += lenOfPath(path)
+        #paths_len += lenOfPath(path)
       else:
         roads.append(path)
+        road = LineString(reproject(path))
         if tags.has_key('lanes'):
           # Lanes present => compute width
           width = int(tags['lanes'])*3.0
@@ -104,101 +142,129 @@ for e in data.getroot():
           width = 3.0
         else:
           width = 6.0
-        l = lenOfPath(path)
-        roads_aera += l * width
-        roads_len += l
+        roadssh.append(road.buffer(width/2))
+        #l = lenOfPath(path)
+        #roads_aera += l * width
+        #roads_len += l
     if tags.has_key('building'):
-      buildings.append(path)
+      buildings.append(Polygon(reproject(path)))
     if tags.has_key('natural'):
       if tags['natural'] in ('forest','wood','scrub'):
-        forests.append(path)
+        forests.append(Polygon(reproject(path)))
       elif tags['natural']=='water':
-        lakes.append(path)
+        lakes.append(Polygon(reproject(path)))
       else:
         print tags['natural']
     if tags.has_key('landuse'):
       if tags['landuse'] in ('forest', 'meadow', 'grass'):
-        forests.append(path)
+        forests.append(Polygon(reproject(path)))
       elif tags['landuse'] in ('farmyard', 'farmland', 'orchard'):
-        farming.append(path)
+        farming.append(Polygon(reproject(path)))
       elif tags['landuse'] in ('village_green', 'recreation_ground'):
-        fake_green.append(path)
+        fake_green.append(Polygon(reproject(path)))
       elif tags['landuse']=='basin':
-        reservoirs.append(path)
+        reservoirs.append(Polygon(reproject(path)))
       elif tags['landuse'] in ('residential','commercial','industrial','construction','retail','quarry'):
-        built_zones.append(path)
+        built_zones.append(Polygon(reproject(path)))
       else:
         print tags['landuse']
     if tags.get('leisure')=='golf_course':
-      golfs.append(path)
+      golfs.append(Polygon(reproject(path)))
+    if tags.get('leisure')=='pitch':
+      stades.append(Polygon(reproject(path)))
+    if tags.get('leisure')=='swimming_pool':
+      reservoirs.append(Polygon(reproject(path)))
     if tags.has_key('waterway'):
       rivers.append(path)
     if tags.get('amenity')=='parking' or tags.get('landuse')=='garages':
-      parkings.append(path)
+      parkings.append(Polygon(reproject(path)))
     if tags.get('name')=="Technopole de Sophia-Antipolis":
       bounds = path
-
-def unions(polygons):
-  return map(lambda x:list(x.exterior.coords),cascaded_union(map(lambda x:Polygon(x),polygons)).geoms)
-
-def withinUnion(polygons, bounds):
-  b = Polygon(reproject(bounds))
-  u = cascaded_union(map(lambda x:Polygon(reproject(x)).intersection(b),polygons))
-  return u
-
-def withinUnionAera(polygons, bounds):
-  u = withinUnion(polygons, bounds)
-  return sum(map(lambda x:x.area,u))
-
-def areaOfPolygons(ps,bounds):
-  print sum(map(areaOfPolygon,ps))
-  print sum(map(areaOfPolygon,within(ps,bounds)))
-  return sum(map(areaOfPolygon,unions(ps)))
+  elif e.tag=='relation':
+    tags = {}
+    outers = []
+    inners = []
+    for i in e:
+      if i.tag=='member' and i.get('role')=='outer':
+        outers.append(i.get('ref'))
+      elif i.tag=='member' and i.get('role')=='inner':
+        inners.append(i.get('ref'))
+      elif i.tag=='tag':
+        tags[i.attrib['k']] = i.attrib['v']
+    if tags.get('landuse') in ('forest', 'meadow', 'grass'):
+      #print 'here',outers,inners
+      #TODO: some ref not found
+      outers = filter(lambda x:x!=None,[ways.get(ref) for ref in outers])
+      inners = filter(lambda x:x!=None,[ways.get(ref) for ref in inners])
+      forest = cascaded_union(map(lambda outer:Polygon(reproject(outer)),outers)).difference(cascaded_union(map(lambda outer:Polygon(reproject(outer)),inners)))
+      forests.append(forest)
 
 if len(nd_not_found)>0:
   print 'WARNING: somes nodes were not found'
 
+print 'Recoupement et regroupements...'
+
+boundsshapely = Polygon(reproject(bounds))
+
+roadssh = cascaded_union(roadssh).intersection(boundsshapely)
+
+built_zones = cascaded_union(built_zones).intersection(boundsshapely)
+buildings = cascaded_union(buildings).intersection(boundsshapely)
+parkings = cascaded_union(parkings).intersection(boundsshapely)
+urbanisation = cascaded_union([built_zones,buildings,parkings,roadssh])
+
+fake_green = cascaded_union(fake_green).intersection(boundsshapely)
+golfs = cascaded_union(golfs).intersection(boundsshapely)
+farming = cascaded_union(farming).intersection(boundsshapely)
+reservoirs = cascaded_union(reservoirs).intersection(boundsshapely)
+stades = cascaded_union(stades).intersection(boundsshapely)
+artificial_green = cascaded_union([fake_green,golfs,farming,reservoirs,stades])
+
+forests = cascaded_union(forests).intersection(boundsshapely)
+lakes = cascaded_union(lakes).intersection(boundsshapely)
+natural = cascaded_union([forests,lakes])
+
+natural = natural.difference(urbanisation) # Mainly for removing roads in forests
+
+print 'Sauvegarde...'
+
+togeojson()
+
 paths_len2 = 0.0
 for path in paths:
-  paths_len2 += lenOfPathWithin(path,bounds)
+  paths_len2 += lenOfPathWithinPolygon(path,boundsshapely)
 
-urbanisation = withinUnion(built_zones+buildings+parkings,bounds)
 roads_len2 = 0.0
 for road in roads:
-  roads_len2 += lenOfPathWithinPolygon(road,urbanisation)
+  roads_len2 += lenOfPathWithinPolygon(road,boundsshapely)
 
-print 'Correction des erreurs (a ignorer si moins d\'un kilometre)'
-print '  Chemins hors frontieres = %d m'%(paths_len-paths_len2)
-print '  Routes hors urbanisation= %d m'%(roads_len-roads_len2)
 print
 print 'Lineaire:'
 print
-print '  chemins et pistes = %d km'%(paths_len2/1000.0)
-print '  rivieres et canaux = %d km'%(sum(map(lenOfPath,rivers))/1000.0)
-print
-print '  routes = %d km %d ha'%(roads_len/1000.0,roads_aera/10000.0)
+print '  chemins et pistes = %.1f km'%(paths_len2/1000.0)
+print '  rivieres et canaux = %.1f km'%(sum(map(lenOfPath,rivers))/1000.0)
+print '  routes = %.1f km'%(roads_len2/1000.0)
 print
 print 'Superficies:'
 print
-print '  Urbanisation: %d ha' % (withinUnionAera(built_zones+buildings+parkings,bounds)/10000.0)
-print '    dont batiments = %d ha'%(withinUnionAera(buildings,bounds)/10000.0)
-print '    dont parkings = %d ha'%(withinUnionAera(parkings,bounds)/10000.0)
+print '  Urbanisation = %.0f ha' % (urbanisation.area/10000.0)
+print '    dont batiments = %.0f ha (%.1f%%)'%(buildings.area/10000.0,buildings.area*100.0/boundsshapely.area)
+print '    dont parkings = %.0f ha'%(parkings.area/10000.0)
+print '    dont routes = %.0f ha'%(roadssh.area/10000.0)
 print
-print '  Espaces verts artificiels: %d ha' % (withinUnionAera(fake_green+golfs+farming+reservoirs,bounds)/10000.0)
-print '    dont golfs = %d ha'%(withinUnionAera(golfs,bounds)/10000.0)
-print '    dont agricole = %d ha'%(withinUnionAera(farming,bounds)/10000.0)
-print '    dont espaces verts = %d ha'%(withinUnionAera(fake_green,bounds)/10000.0)
-print '    dont reservoirs = %d ha'%(withinUnionAera(reservoirs,bounds)/10000.0)
+print '  Espaces verts artificiels = %.0f ha' % (artificial_green.area/10000.0)
+print '    dont golfs = %.1f ha'%(golfs.area/10000.0)
+print '    dont agricole = %.1f ha'%(farming.area/10000.0)
+print '    dont espaces verts = %.1f ha'%(fake_green.area/10000.0)
+print '    dont reservoirs et piscines = %.1f ha'%(reservoirs.area/10000.0)
+print '    dont terrains de sport (hors golf) = %.1f ha'%(stades.area/10000.0)
 print
-print '  Espaces naturels = %d ha'%(withinUnionAera(forests+lakes,bounds)/10000.0)
-print '    dont foret, garrigue, prairie = %d ha'%(withinUnionAera(forests,bounds)/10000.0)
-print '    dont eau (lacs, etangs), hors rivieres etroites = %d ha'%(withinUnionAera(lakes,bounds)/10000.0)
+print '  Espaces naturels = %.0f ha'%(natural.area/10000.0)
+print '    dont foret, garrigue, prairie = %.0f ha'%(forests.area/10000.0)
+print '    dont eau (lacs, etangs), hors rivieres etroites = %.0f ha'%(lakes.area/10000.0)
 print
-print 'superficie totale = %d ha'%(areaOfPolygon(bounds)/10000.0)
-print 'superficie totale identifiee = %d ha'%(withinUnionAera(built_zones+buildings+parkings+fake_green+golfs+farming+reservoirs+forests+lakes,bounds)/10000.0)
+print '  Non identifiee = %.0f ha'%((boundsshapely.area-cascaded_union([urbanisation,artificial_green,natural]).area)/10000.0)
+print
+print 'superficie totale = %.0f ha'%(boundsshapely.area/10000.0)
 
-#TODO: routes a compter dans urbanisation
-#TODO: restreindre les donnees lineaiares a l'interieur de sophia
 #TODO: donner une largeur aux rivieres?
-
-
